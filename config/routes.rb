7280ead548d918c29fd6d0e58c1ae50e8f8d815c
David Pan <pan2022@berkeley.edu>
2019-03-04 17:00:57 -0800

    added two controllers: home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+  get 'about/index'
   devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'welcome#index'
+  root :to => 'home#index'
 end
